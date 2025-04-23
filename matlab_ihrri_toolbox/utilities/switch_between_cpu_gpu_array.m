0e949914725e436e85ff7d902fb0a67cb79bff63
Fabien Momey <fabien.momey@univ-st-etienne.fr>
2025-04-23 13:51:08 +0200

    First function of the framework for using the toolbox with a GPU device.

---
+++ matlab_ihrri_toolbox/utilities/switch_between_cpu_gpu_array.m
@@ -0,0 +1,53 @@
+
+function [x_output] = switch_between_cpu_gpu_array(x_input)
+% [x_output] = switch_between_cpu_gpu_array(x_input)
+%
+% Function to switch an array from the "CPU space" to the "GPU space", and
+% vice-versa.
+%
+% Created: 04/23/2025 (mm/dd/yyyy)
+% Author:   Fabien Momey
+%           Laboratoire Hubert Curien UMR CNRS 5516,
+%           Université Jean Monnet,
+%           F-42000 Saint-Étienne,
+%           France
+%           fabien.momey@univ-st-etienne.fr
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (c) 2020, Fabien Momey
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 
+% 1. Redistributions of source code must retain the above copyright notice, this
+%    list of conditions and the following disclaimer.
+% 
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+%    this list of conditions and the following disclaimer in the documentation
+%    and/or other materials provided with the distribution.
+% 
+% 3. Neither the name of the copyright holder nor the names of its
+%    contributors may be used to endorse or promote products derived from
+%    this software without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+% OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if (~isempty(gpuDevice))
+    if (isgpuarray(x_input))
+        x_output = gather(x_input) ;
+    else
+        x_output = gpuArray(x_input) ;
+    end
+end
+end
